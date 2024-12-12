4845b00849f55ddc356cebe60b2ff9f240e1006d
RohitMane84869 <139763063+RohitMane84869@users.noreply.github.com>
2024-12-12 10:45:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Dynamic date in the footer
+document.querySelector('footer p').innerText += ` - ${new Date().getFullYear()}`;
